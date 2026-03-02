e553fc4b87e8875b117cc7a28f6f67c27df1e1db
andornaut <andornaut@users.noreply.github.com>
2026-03-02 13:57:15 -0500

    Update src/helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -2,10 +2,11 @@ export const cycle = (length, index) => {
   if (index >= length) {
     return 0;
   }
-  if (index < 0) {
+  if ( < 0) {
     return length - 1 || 0;
   }
   return index;
 };
 
 export const isFile = url => !url.endsWith('/');
+index
